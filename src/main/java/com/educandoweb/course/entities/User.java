2e73233975d0a0adb30ce244ede8a11c4b5987aa
Felipe Lustosa <lustosaf590@gmail.com>
2024-10-17 15:37:55 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,17 @@
 package com.educandoweb.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity //você está dizendo ao JPA que essa classe representa uma tabela no banco de dados, e cada instância dessa classe será um registro (linha) nessa tabela.
+
+@Table(name = "tb_user")//O nome da tabela será, por padrão, o nome da classe. No caso, será User (mas você pode alterar o nome da tabela com a anotação @Table).
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    @Id // Define o campo id como a chave primária da tabela.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Define como o valor da chave primária (ID) será gerado. O valor GenerationType.IDENTITY informa ao banco de dados que ele deve ser responsável por gerar esse valor (geralmente usando auto-incremento).
     private Long id;
     private String name;
     private String email;
